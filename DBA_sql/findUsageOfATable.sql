18d62b718cd0eecbd18aa43a9ec26d7a1ec3c9c0
Boe Bruns <boe.bruns@gmail.com>
2021-04-13 13:07:35 -0500

    update

--- DBA_sql/findUsageOfATable.sql
+++ DBA_sql/findUsageOfATable.sql
@@ -1,4 +1,5 @@
 select pg_user.usename, userid, query, calls
 from pg_stat_statements, pg_user
-where query like '%active_jobs%'
+where lower(query) like '%active_jobs%'
 and pg_user.usesysid = userid
+order by calls desc
